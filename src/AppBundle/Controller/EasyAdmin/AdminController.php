4a31d3d5bd2ded25d579a0302b2c29e84db54fd1
petrero <petrero@gmail.com>
2017-08-20 11:23:36 +0300

    Copy code from branch 16

---
+++ src/AppBundle/Controller/EasyAdmin/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Controller\EasyAdmin;
+
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use AppBundle\Entity\Genus;
+
+class AdminController extends BaseAdminController {
+  public function changePublishedStatusAction(){
+    $id = $this->request->query->get('id');
+    $entity = $this->em->getRepository(Genus::class)->find($id);
+    
+    $entity->setIsPublished(!$entity->getIsPublished());
+    $this->em->flush();
+    
+    $this->addFlash('success', sprintf('Genus %spublished', $entity->getIsPublished() ? '' : 'un'));
+    
+    return $this->redirectToRoute('easyadmin', [
+      'action' => 'show',
+      'entity' => $this->request->query->get('entity'),
+      'id' => $id
+    ]);
+  }
+}
